cdae548e3a99c508c235fb4898672ae3475b88e7
Emilio Bensignor <liobensignor@gmail.com>
2025-05-30 15:41:40 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
-import { use, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import { Routes, Route, NavLink } from 'react-router-dom'
+
+import Home from './views/Home';
+import Products from './views/Products';
+import Contact from './views/Contact';
+import NotFound from './views/NotFound';
+
+import Header from './components/Header';
 
 function App() {
-  const [usuario, setUsuario] = useState({name: 'Juan', age: 30});
-  const [products, setProduct] = useState([
-    { id: 1, nombre: 'Producto 1', precio: 100 },
-    { id: 2, nombre: 'Producto 2', precio: 200 },
-    { id: 3, nombre: 'Producto 3', precio: 300 }
-  ]);
-
-  function cambiarNombre() {
-    setUsuario({...usuario, name: 'Micaela'});
-  }
-
-  function nuevoProducto() {
-    const id = products.length + 1;
-    const nuevo = {
-      id,
-      nombre: 'Producto ' + id,
-      precio: 100 * id
-    };
-    setProduct([...products, nuevo]);
-  }
 
   return (
     <>
-      <h1>Hooks</h1>
-      <p>Bienvenido {usuario.name}</p>
-      <p>Edad: {usuario.age}</p>
-      <button onClick={cambiarNombre} type='button'>Cambiar nombre</button>
-      <button onClick={nuevoProducto} type='button'>Nuevo producto</button>
-      <div>
-        {products.map(product => (
-          <div key={product.id}>
-            <h2>{product.nombre}</h2>
-            <p>Precio: {product.precio}</p>
-          </div>
-        ))}
-      </div>
+      <Header />
+      <nav>
+        <ul>
+          <li><NavLink to="/">Inicio</NavLink></li>
+          <li><NavLink to="/products">Productos</NavLink></li>
+          <li><NavLink to="/contact">Contacto</NavLink></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
